3c1d600f6e71bb7983ea50cf1466937aa2eb4f76
Ryan Kirsch <agalloch88@gmail.com>
2022-03-23 13:29:16 -0400

    address vercel streaming limitation with async/await and promises

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,35 +1,52 @@
-export default function (req, res) {
-    console.log(req.body);
+export default async (req, res) => {
+  console.log(req.body);
 
-    require('dotenv').config();
-    const PASSWORD = process.env.password;
-    let nodeMailer = require('nodemailer');
-    const transporter = nodeMailer.createTransport({
-        port: 465,
-        host: 'smtp.gmail.com',
-        auth: {
-            user: 'rosebudturnerdotcom@gmail.com',
-            pass: PASSWORD,
-        },
-        secure: true,
+  require("dotenv").config();
+  const PASSWORD = process.env.password;
+  let nodeMailer = require("nodemailer");
+  const transporter = nodeMailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: "rosebudturnerdotcom@gmail.com",
+      pass: PASSWORD,
+    },
+    secure: true,
+  });
+
+  await new Promise((resolve, reject) => {
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
     });
+  });
 
-    const mailData = {
-        from: 'rosebudturnerdotcom@gmail.com',
-        to: ['ryankirsch88@gmail.com', 'agalloch88@gmail.com'],
-        subject: `Message From ${req.body.name}`,
-        text: req.body.message + " | Sent from: " + req.body.email,
-        html: `<div>${req.body.message}</div><p>Sent from:
+  const mailData = {
+    from: "rosebudturnerdotcom@gmail.com",
+    to: ["ryankirsch88@gmail.com", "agalloch88@gmail.com"],
+    subject: `Message From ${req.body.name}`,
+    text: req.body.message + " | Sent from: " + req.body.email,
+    html: `<div>${req.body.message}</div><p>Sent from:
         ${req.body.email}</p>`,
-    }
+  };
 
+  await new Promise((resolve, reject) => {
     transporter.sendMail(mailData, function (err, info) {
-        if (err)
-            console.log(err)
-        else
-            console.log(info);
-    })
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+  });
 
-    res.status(200)
-    res.send()
-}
+  res.status(200).json({ status: "OK" });
+  res.send();
+};
