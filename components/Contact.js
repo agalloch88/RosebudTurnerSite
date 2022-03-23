e4cc08a30ba1df617a81050372ad48a7eab73853
Ryan Kirsch <agalloch88@gmail.com>
2022-03-23 11:21:29 -0400

    create state vars for contact form, hook up to inputs, create handleSubmit function

--- components/Contact.js
+++ components/Contact.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import userData from "@constants/data";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("sending email...");
+
+    let data = {
+      name: name,
+      email: email,
+      message: message,
+    }
+
+    fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then((res) => {
+      console.log('response received');
+      if (res.status === 200) {
+        console.log('response success!');
+        setSubmitted(true);
+        setName("");
+        setEmail("");
+        setMessage("");
+      }
+    })
+  }
+
   return (
     <section>
       <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800 antialiased">
@@ -148,6 +182,7 @@ export default function Contact() {
             </label>
             <input
               type="text"
+              onChange={(e) => {setName(e.target.value)}}
               className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
               name="name"
             />
@@ -156,6 +191,7 @@ export default function Contact() {
             </label>
             <input
               type="text"
+              onChange={(e) => {setEmail(e.target.value)}}
               className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
               name="email"
             />
@@ -168,11 +204,13 @@ export default function Contact() {
             <textarea
               rows="4"
               type="text"
+              onChange={(e) => {setMessage(e.target.value)}}
               className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
               name="message"
             ></textarea>
             <button
               type="submit"
+              onClick={(e) => {handleSubmit(e)}}
               className="bg-blue-500 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs font-bold"
             >
               Send Message
